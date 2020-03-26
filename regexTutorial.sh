09f8208be546678c4a77e625aafd228b6a10690f
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-26 07:23:28 +0530

    ADDED: Basic regex command

---
+++ regexTutorial.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+#Basic Regular expressions:- 
+	*) Some of the commonly used commands with Regular expressions are 
+		tr, sed, vi and grep.Listed below are some of the basic Regex.
+==================================================================
+Symbol	||			Descriptions			||
+==================================================================
+.	||		replaces any character			||
+----------------------------------------------------------------||
+^	||		matches start of string			||
+----------------------------------------------------------------||
+$	||		matches end of string			||
+----------------------------------------------------------------||
+*	||matches up zero or more times the preceding character	||
+----------------------------------------------------------------||
+\	||		Represent special characters		||
+----------------------------------------------------------------||
+()	||		Groups regular expressions		||
+----------------------------------------------------------------||
+?	||		Matches up exactly one character	||
+==================================================================
