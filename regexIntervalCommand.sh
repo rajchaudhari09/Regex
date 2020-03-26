a4669d19c3180957933745630b231e40c3e70adb
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-26 07:32:53 +0530

    ADDED: Interval regex command

---
+++ regexIntervalCommand.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+#Interval Regular expressions:-
+	*)These expressions tell us about the number of occurrences of a 
+		character in a string. They are
+==========================================================================
+Expression ||			Description				||
+--------------------------------------------------------------------------
+{n}	   ||	Matches the preceding character appearing 'n' times 	||
+	   ||	exactly							||
+--------------------------------------------------------------------------
+{n,m}	   ||	Matches the preceding character appearing 'n' times 	||
+	   ||	but not more than m					||
+--------------------------------------------------------------------------
+{n,}	   ||	Matches the preceding character 			||
+	   ||	only when it appears 'n' times or more			||
+==========================================================================
